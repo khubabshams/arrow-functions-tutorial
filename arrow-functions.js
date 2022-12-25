fdf2262cdb8e0ef3eb0a0cf9187948a472179d71
Khubab <khubab220@gmail.com>
2022-12-25 11:28:48 +0000

    arrow function forms

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,41 @@
  */
 
 // Vanilla JavaScript Function
+function addTwoNumbersVan(a, b) {
+    return a + b;
+}
+
+let sum1 = addTwoNumbersVan(2, 5);
+console.log(sum1);
 
 // Arrow Function With Parameters
+const addTwoNumbersArr = (a, b) => {
+    return a + b;
+}
+let sum2 = addTwoNumbersArr(2, 5);
+console.log(sum2);
 
 // Single Line Arrow Function With Parameters
+// const addTwoNumbersArrSing = (a, b) => (a + b);
+const addTwoNumbersArrSing = (a, b) => a + b;
+let sum3 = addTwoNumbersArrSing(2, 5);
+console.log(sum3);
 
 // Implicit Returns
 
+const saySomething = message => console.log(message);
+saySomething("HELLO!");
+
+const sayHello = () => console.log("HELLO!")
+sayHello();
+
+
 // Returning Multiple Lines
+
+const returnMultibleLines = () => (
+    `<p>
+        Hello
+    </p>
+    `
+);
+console.log(returnMultibleLines());
